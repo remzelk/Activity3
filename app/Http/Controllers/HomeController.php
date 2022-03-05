2fdf7dc51a9575a3090d407b06eb3d18172f5807
remzelk <aliciamaye.mondero@letran.edu.ph>
2022-03-06 02:51:56 +0800

    e

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -70,7 +70,7 @@ class HomeController extends Controller
     public function destroy($id)
     {
         $this->students[$id] -> delete();
-        return view('Home.index', ['students'=>$this->students[$id]]);
+        return view('Home.index', ['students'=>$this->students]);
     }
     
    
